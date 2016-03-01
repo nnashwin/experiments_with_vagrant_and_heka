7a0d609e69d2dd13ddf705b9ba16d203c905046a
ttymed <tyler.boright@gmail.com>
2016-03-01 14:19:09 +0800

    set up prometheus, the diskstats exporter, and grafana to automatically build/setup.  returning back to the heka decoders to work out a final configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,14 +8,15 @@ sudo apt-get update
 
 sudo apt-get install -y syslog-ng-core syslog-ng nodejs npm git haproxy cmake tcl8.5 redis-tools
 
-sudo apt-get install build-essential
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+touch /etc/apt/sources.list.d/docker.list
+
+sudo apt-get install -y build-essential apt-transport-https ca-certificates docker.io
 
 sudo locale-gen UTF-8
 
-sudo apt-get install cmake
-
 sudo ln -s /usr/bin/nodejs /usr/bin/node
-
 
 echo PATH $PATH
 [ -f ~/.bash_profile ] || touch ~/.bash_profile
@@ -24,38 +25,70 @@ echo PATH $PATH
 
 sudo wget https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
 
-sudo mkdir /usr/go && sudo tar -zxf go1.4.2.linux-amd64.tar.gz -C /usr/go --strip-components 1
-sudo cp -R /usr/go /usr/local/go
+sudo tar -zxf go1.4.2.linux-amd64.tar.gz -C ~/go --strip-components 1
+mkdir work
 
-echo 'export PATH=/usr/go/bin:$PATH' >> ~/.bash_profile
+#set path and root of go
+echo 'export GOPATH=$HOME/work' >> ~/.bash_profile
+echo 'export GOROOT=$HOME/go' >> ~/.bash_profile
+
+echo 'export PATH=$PATH:$GOROOT/bin' >> ~/.bash_profile
+
+# download dependencies needed for the heka_exporter
+go get github.com/prometheus/client_golang/prometheus
+go get github.com/stretchr/stew/objects
+
+# git clone exporter
+git clone https://github.com/docker-infra/heka_exporter.git $HOME/heka_exporter
+
+# go build the binary for the exporter
+go build .
+
+# download 
 
 # download and untar heka
 
-wget https://github.com/mozilla-services/heka/releases/download/v0.10.0/heka-0_10_0-linux-amd64.tar.gz
+sudo wget https://github.com/mozilla-services/heka/releases/download/v0.10.0/heka-0_10_0-linux-amd64.tar.gz
 sudo mkdir /usr/heka/ && sudo tar -xzf heka-0_10_0-linux-amd64.tar.gz -C /usr/heka/ --strip-components 1
 
 #download and untar redis
 
-wget http://download.redis.io/releases/redis-3.0.7.tar.gz
+sudo wget http://download.redis.io/releases/redis-3.0.7.tar.gz
 sudo mkdir /usr/redis && sudo tar -xzf redis-3.0.7.tar.gz -C /usr/redis/ --strip-components 1
 cd /usr/redis && sudo make
 
-# Download and untar prometheus
+# Download and untar, and copy config file for prometheus
 
-wget https://github.com/prometheus/prometheus/releases/download/0.17.0rc2/prometheus-0.17.0rc2.linux-amd64.tar.gz
+sudo wget https://github.com/prometheus/prometheus/releases/download/0.17.0rc2/prometheus-0.17.0rc2.linux-amd64.tar.gz
 sudo mkdir /usr/prometheus && sudo tar -xzf prometheus-0.17.0rc2.linux-amd64.tar.gz -C /usr/prometheus --strip-components 1
+sudo cp /vagrant_data/prometheus.yml /usr/prometheus/prometheus.yml
+
+# Download and untar graphana
+sudo wget https://grafanarel.s3.amazonaws.com/builds/grafana-2.5.0.linux-x64.tar.gz
+sudo mkdir /usr/grafana && sudo tar -xzf grafana-2.5.0.linux-x64.tar.gz -C /usr/grafana/ --strip-components 1
+# copy grafana config to the correct dir
+sudo cp /vagrant_data/grafana_conf.ini /usr/grafana/conf/custom.ini
+
+#create dir and copy json files to upload dashboards into grafana
+sudo mkdir /usr/grafana/dashboard_json
+sudo cp /vagrant_data/grafana_dashboards/* /usr/grafana/dashboard_json/
+
+# clone exporter repo into the file system
+
+sudo mkdir /usr/heka_exporter
+sudo git clone https://github.com/docker-infra/heka_exporter.git /usr/heka_exporter
+
 
 echo 'export PATH=/usr/heka/bin:$PATH' >> ~/.bash_profile
 
 sudo mkdir /usr/share/heka/
 sudo mkdir /usr/share/heka/lua_decoders
+sudo mkdir /usr/share/heka/lua_filters
+sudo mkdir /usr/share/heka/lua_modules
 
 sudo mkdir /var/log/syslog-ng
 
 sudo mkdir /home/heka
-
-
-
 
 # config heka
 
@@ -63,24 +96,47 @@ echo PATH $PATH
 
 sudo mkdir /usr/heka/log/
 
-sudo cp /vagrant/heka-config.toml /etc/hekad.toml
+
+sudo cp /vagrant_data/heka-config.toml /etc/hekad.toml
 
 sudo cp /vagrant/haproxy_decoder.lua /usr/heka/share/heka/lua_decoders/haproxy_decoder.lua
 sudo cp /vagrant/redis_decoder.lua /usr/heka/share/heka/lua_decoders/redis_decoder.lua
 
-sudo cp /vagrant/haproxy_decoder.lua /usr/share/heka/lua_decoders/haproxy_decoder.lua
-sudo cp /vagrant/redis_decoder.lua /usr/share/heka/lua_decoders/redis_decoder.lua
+sudo cp /usr/heka/share/heka/lua_decoders/* /usr/share/heka/lua_decoders/
+sudo cp /usr/heka/share/heka/lua_filters/* /usr/share/heka/lua_filters/
+sudo cp /usr/heka/share/heka/lua_modules/* /usr/share/heka/lua_modules/
+
+# config heka dashboard to send to prometheus
+sudo mkdir /usr/share/heka/dasher
+
+# create dir for and clone comp stats exporter
+sudo mkdir /usr/node_exporter
+sudo git clone https://github.com/prometheus/node_exporter.git /usr/node_exporter
+cd /usr/node_exporter
+sudo make
+
 
 
 sudo chmod 755 /home
 sudo chmod 777 /home/heka
 sudo chmod 777 /home/heka
 
+# copy syslog-ng conf to the appropriate directory
+
 sudo mv /etc/syslog-ng/syslog-ng.conf /etc/syslog-ng/syslog-ng.conf.default
-sudo mv /vagrant/syslog-ng.conf /etc/syslog-ng/syslog-ng.conf
+sudo cp /vagrant_data/syslog-ng.conf /etc/syslog-ng/syslog-ng.conf
 
-# config systemd with two custom services: heka and syslog-ng
-sudo cp /vagrant/syslog-ng.service /etc/systemd/system/syslog-ng.service
-sudo cp /vagrant/heka.service /etc/systemd/system/heka.service
+# config systemd custom logging services
+sudo systemctl enable /vagrant_data/syslog-ng.service
+sudo systemctl enable /vagrant_data/heka.service
+sudo systemctl enable /vagrant_data/prometheus.service
+sudo systemctl enable /vagrant_data/node-exporter.service
+sudo systemctl enable /vagrant_data/grafana.service
 
-# download prometheus binaries
+
+#start systemctl services
+sudo systemctl start syslog-ng.service
+sudo systemctl start heka.service
+sudo systemctl start prometheus.service
+sudo systemctl start node-exporter.service
+sudo systemctl start grafana.service
