40680bfa37a690d4eaf8b354cdda96096b658225
ttymed <tyler.boright@gmail.com>
2016-03-02 18:21:48 +0800

    got prometheus to scrape from consul the correct paths/ports based on the service names.  Next need to match up with luowei and get it working in production

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,15 +6,13 @@ sudo add-apt-repository ppa:george-edison55/cmake-3.x
 
 sudo apt-get update
 
-sudo apt-get install -y syslog-ng-core syslog-ng nodejs npm git haproxy cmake tcl8.5 redis-tools
+sudo apt-get install -y syslog-ng-core syslog-ng nodejs npm git haproxy cmake tcl8.5 redis-tools unzip
 
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 touch /etc/apt/sources.list.d/docker.list
 
 sudo apt-get install -y build-essential apt-transport-https ca-certificates docker.io
-
-sudo locale-gen UTF-8
 
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 
@@ -57,15 +55,21 @@ sudo wget http://download.redis.io/releases/redis-3.0.7.tar.gz
 sudo mkdir /usr/redis && sudo tar -xzf redis-3.0.7.tar.gz -C /usr/redis/ --strip-components 1
 cd /usr/redis && sudo make
 
-# Download and untar, and copy config file for prometheus
+# Download, untar, and copy config file for prometheus
 
 sudo wget https://github.com/prometheus/prometheus/releases/download/0.17.0rc2/prometheus-0.17.0rc2.linux-amd64.tar.gz
 sudo mkdir /usr/prometheus && sudo tar -xzf prometheus-0.17.0rc2.linux-amd64.tar.gz -C /usr/prometheus --strip-components 1
 sudo cp /vagrant_data/prometheus.yml /usr/prometheus/prometheus.yml
 
+# Download and untar consul, copying it into /usr/local/bin
+sudo wget https://releases.hashicorp.com/consul/0.6.3/consul_0.6.3_linux_amd64.zip
+sudo unzip consul_0.6.3_linux_amd64.zip
+sudo mv consul /usr/local/bin/
+
 # Download and untar graphana
 sudo wget https://grafanarel.s3.amazonaws.com/builds/grafana-2.5.0.linux-x64.tar.gz
 sudo mkdir /usr/grafana && sudo tar -xzf grafana-2.5.0.linux-x64.tar.gz -C /usr/grafana/ --strip-components 1
+
 # copy grafana config to the correct dir
 sudo cp /vagrant_data/grafana_conf.ini /usr/grafana/conf/custom.ini
 
